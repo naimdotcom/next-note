82e6731d55c7fb35ea425e059a8cc7035c73c772
Naim AK <149870242+naimak659@users.noreply.github.com>
2025-01-31 00:50:00 +0600

    refactor: remove Navbar and dashboard components, update layout with Sidebar and Breadcrumbs

--- src/app/(pages)/u/layout.tsx
+++ src/app/(pages)/u/layout.tsx
@@ -1,6 +1,19 @@
-import Navbar from "@/components/common/Navbar";
 import type { Metadata } from "next";
-
+import { AppSidebar } from "@/components/app-sidebar";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Separator } from "@/components/ui/separator";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
 export const metadata: Metadata = {
   title: "notes of user",
   description: "",
@@ -12,11 +25,30 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="container mx-auto">
-      <div>
-        <Navbar />
-      </div>
-      <div>{children}</div>
-    </div>
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbLink href="#">
+                    Building Your Application
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className="hidden md:block" />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+        </header>
+        <div className="mx-7">{children}</div>
+      </SidebarInset>
+    </SidebarProvider>
   );
 }
--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,23 +1,23 @@
-"use client"
+"use client";
 
-import * as React from "react"
+import * as React from "react";
 import {
   BookOpen,
   Bot,
   Command,
   Frame,
   LifeBuoy,
-  Map,
-  PieChart,
   Send,
   Settings2,
+  Trash2,
   SquareTerminal,
-} from "lucide-react"
+  Archive,
+} from "lucide-react";
 
-import { NavMain } from "@/components/nav-main"
-import { NavProjects } from "@/components/nav-projects"
-import { NavSecondary } from "@/components/nav-secondary"
-import { NavUser } from "@/components/nav-user"
+import { NavMain } from "@/components/nav-main";
+import { NavProjects } from "@/components/nav-projects";
+import { NavSecondary } from "@/components/nav-secondary";
+import { NavUser } from "@/components/nav-user";
 import {
   Sidebar,
   SidebarContent,
@@ -26,133 +26,80 @@ import {
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
-} from "@/components/ui/sidebar"
-
-const data = {
-  user: {
-    name: "shadcn",
-    email: "m@example.com",
-    avatar: "/avatars/shadcn.jpg",
-  },
-  navMain: [
-    {
-      title: "Playground",
-      url: "#",
-      icon: SquareTerminal,
-      isActive: true,
-      items: [
-        {
-          title: "History",
-          url: "#",
-        },
-        {
-          title: "Starred",
-          url: "#",
-        },
-        {
-          title: "Settings",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Models",
-      url: "#",
-      icon: Bot,
-      items: [
-        {
-          title: "Genesis",
-          url: "#",
-        },
-        {
-          title: "Explorer",
-          url: "#",
-        },
-        {
-          title: "Quantum",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Documentation",
-      url: "#",
-      icon: BookOpen,
-      items: [
-        {
-          title: "Introduction",
-          url: "#",
-        },
-        {
-          title: "Get Started",
-          url: "#",
-        },
-        {
-          title: "Tutorials",
-          url: "#",
-        },
-        {
-          title: "Changelog",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Settings",
-      url: "#",
-      icon: Settings2,
-      items: [
-        {
-          title: "General",
-          url: "#",
-        },
-        {
-          title: "Team",
-          url: "#",
-        },
-        {
-          title: "Billing",
-          url: "#",
-        },
-        {
-          title: "Limits",
-          url: "#",
-        },
-      ],
-    },
-  ],
-  navSecondary: [
-    {
-      title: "Support",
-      url: "#",
-      icon: LifeBuoy,
-    },
-    {
-      title: "Feedback",
-      url: "#",
-      icon: Send,
-    },
-  ],
-  projects: [
-    {
-      name: "Design Engineering",
-      url: "#",
-      icon: Frame,
-    },
-    {
-      name: "Sales & Marketing",
-      url: "#",
-      icon: PieChart,
-    },
-    {
-      name: "Travel",
-      url: "#",
-      icon: Map,
-    },
-  ],
-}
-
+} from "@/components/ui/sidebar";
+// const data = ;
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  const [data, setData] = React.useState({
+    user: {
+      name: "shadcn",
+      email: "m@example.com",
+      avatar: "/avatars/shadcn.jpg",
+      items: [],
+    },
+    navMain: [
+      {
+        title: "Settings",
+        url: "#",
+        icon: Settings2,
+      },
+    ],
+    navSecondary: [
+      {
+        title: "Archive",
+        url: "#",
+        icon: Archive,
+      },
+      {
+        title: "Trash",
+        url: "#",
+        icon: Trash2,
+      },
+      {
+        title: "Support",
+        url: "#",
+        icon: LifeBuoy,
+      },
+      {
+        title: "Feedback",
+        url: "#",
+        icon: Send,
+      },
+    ],
+    projects: [
+      // {
+      //   name: "Design Engineering",
+      //   url: "#",
+      //   icon: Frame,
+      // },
+      // {
+      //   name: "Sales & Marketing",
+      //   url: "#",
+      //   icon: PieChart,
+      // },
+      // {
+      //   name: "Travel",
+      //   url: "#",
+      //   icon: Map,
+      // },
+    ],
+  });
+  React.useEffect(() => {
+    fetch("https://dummyjson.com/users/1")
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+
+        setData((prev) => ({
+          ...prev,
+          user: {
+            ...prev.user,
+            name: res?.username,
+            email: res?.email,
+            avatar: res?.image,
+          },
+        }));
+      });
+  }, []);
   return (
     <Sidebar variant="inset" {...props}>
       <SidebarHeader>
@@ -174,12 +121,12 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
       </SidebarHeader>
       <SidebarContent>
         <NavMain items={data.navMain} />
-        <NavProjects projects={data.projects} />
+        {/* <NavProjects projects={data.projects} /> */}
         <NavSecondary items={data.navSecondary} className="mt-auto" />
       </SidebarContent>
       <SidebarFooter>
         <NavUser user={data.user} />
       </SidebarFooter>
     </Sidebar>
-  )
+  );
 }
